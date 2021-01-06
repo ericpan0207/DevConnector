e8b99af2e2b2714b6d80b77aacdd4059709b7db9
Eric Pan <ericpan0207@gmail.com>
2021-01-05 19:47:48 -0800

    added profile, experience, education components

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,9 @@
-import { GET_PROFILE, PROFILE_ERROR } from "../actions/types";
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+} from "../actions/types";
 
 const initialState = {
   profile: null,
@@ -13,6 +18,7 @@ export const reducer = (state = initialState, action) => {
 
   switch (type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return {
         ...state,
         profile: payload,
@@ -22,6 +28,13 @@ export const reducer = (state = initialState, action) => {
       return {
         ...state,
         error: payload,
+        loading: false,
+      };
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
         loading: false,
       };
     default:
