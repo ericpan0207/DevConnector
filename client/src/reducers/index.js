3bb52c86594e4a8c380cf97736020296f488d53c
Eric Pan <ericpan0207@gmail.com>
2021-01-06 17:55:53 -0800

    project finished

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { reducer as alertReducer } from "./alert";
 import { reducer as authReducer } from "./auth";
 import { reducer as profileReducer } from "./profile";
+import { reducer as postReducer } from "./post";
 
 export const reducer = combineReducers({
   alert: alertReducer,
   auth: authReducer,
   profile: profileReducer,
+  post: postReducer,
 });
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const Post = require("../../models/Post");
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -148,7 +149,8 @@ router.get("/user/:user_id", async (req, res) => {
 // @access  Private
 router.delete("/", auth, async (req, res) => {
   try {
-    // @todo - remove users posts
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
 
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
@@ -321,13 +323,14 @@ router.get("/github/:username", (req, res) => {
   {
     try {
       const options = {
-        uri: `https://api.github.com/users/${
-          req.params.username
-        }/repos?per_page=5&sort=created:asc&client_id=${config.get(
-          "githubClientId"
-        )}&client_secret=${config.get("githubSecret")}`,
+        uri: encodeURI(
+          `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&`
+        ),
         method: "GET",
-        headers: { "user-agent": "node.js" },
+        headers: {
+          "user-agent": "node.js",
+          Authorization: `token ${config.get(`githubToken`)}`,
+        },
       };
 
       request(options, (error, response, body) => {
